b66e304b7a3e14cc47daa2feecd0ba0c20ecf246
ahmedmeshref <a.meshref@alustudent.com>
2020-05-28 02:59:29 +0200

    Add last digit of fib js and py solutions

--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.js
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.js
@@ -16,6 +16,15 @@ function readLine(line) {
 
 function fib(n) {
     // write your code here
+    if (n <= 1) return n
+    let first = 0;
+    let second = 1;
+    let temp;
+    for (let i = 1; i < n; i++){
+        temp = (first + second) % 10;
+        [first, second] = [second, temp];
+    }
+    return second;
 }
 
 module.exports = fib;

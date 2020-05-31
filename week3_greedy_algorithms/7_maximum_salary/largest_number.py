1b463aeb636f34a226a0e16ff8f3cddeb7345ac7
ahmedmeshref <a.meshref@alustudent.com>
2020-05-31 20:41:00 +0200

    delete unneeded java files

--- week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -3,8 +3,8 @@
 
 def IsGreaterOrEqual(num, max_num):
     """
-    IsGreaterOrEqual takes in two numbers and returns true only if the resulting composition of number and
-    max_num in order is greater than or equal to the composition of max_num and num
+    IsGreaterOrEqual takes in two numbers and returns true only if the resulting composition of
+    number and max_num in order is greater than or equal to the composition of max_num and num
     """
     return int(str(num) + str(max_num)) >= int(str(num) + str(max_num))
 
@@ -14,7 +14,8 @@ def largest_number(lst):
     largest_number function takes in a list of integers each of any length and composes the largest
     number out of the given inout integers.
     :param lst: list of numbers
-    :return: string represents the largest (max) composed number that can be arranged out of all given numbers
+    :return: string represents the largest (max) composed number that can be arranged out of all
+    given numbers
     """
     answer = ""
 
@@ -33,3 +34,4 @@ if __name__ == "__main__":
     _ = int(input())
     lst = [int(i) for i in input().split()]
     print(largest_number(lst))
+

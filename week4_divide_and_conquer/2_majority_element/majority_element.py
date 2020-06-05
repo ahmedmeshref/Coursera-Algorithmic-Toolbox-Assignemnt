1457f236da5f1942563e18daab0548831b039a28
ahmedmeshref <a.meshref@alustudent.com>
2020-06-05 07:41:26 +0200

    Add solution for maj_number problem

--- week4_divide_and_conquer/2_majority_element/majority_element.py
+++ week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -1,18 +1,36 @@
-# Uses python3
-import sys
+"""
+Task: The goal in this code problem is to check whether an input sequence contains a majority element.
 
-def get_majority_element(a, left, right):
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
-    return -1
+Input Format: The first line contains an integer 𝑛, the next one contains a sequence of 𝑛 non-negative
+integers 𝑎0, 𝑎1, . . . , 𝑎𝑛−1.
+Constraints. 1 ≤ 𝑛 ≤ 105
+; 0 ≤ 𝑎𝑖 ≤ 109
+for all 0 ≤ 𝑖 < 𝑛.
+
+Output Format: Output 1 if the sequence contains an element that appears strictly more than 𝑛/2 times,
+and 0 otherwise.
+"""
+
+from collections import Counter
+
+
+# O(n) solution
+def get_maj_element(nums, len_nums):
+    """
+    get_maj_element check whether an input sequence contains a majority element which appeared more than len_of_nums/2
+    :param nums: list of integer
+    :param len_nums: integer represents the number of elements in nums
+    :return: 1 if a majority element exists, 0 otherwise
+    """
+    counts = Counter(nums)
+    for ele in counts:
+        if counts[ele] > len_nums / 2:
+            return 1
+    return 0
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
-        print(1)
-    else:
-        print(0)
+    n = int(input())
+    lst = list(map(int, input().split()))
+    # get_maj_element O(n) solution
+    print(get_maj_element(lst, n))

2301ca8c25d2b8f53d631b2b1ca43352daa34b2a
ahmedmeshref <a.meshref@alustudent.com>
2020-06-10 12:54:08 +0200

    Add py solution for 3 partition quicksort

--- week4_divide_and_conquer/3_improving_quicksort/sorting.py
+++ week4_divide_and_conquer/3_improving_quicksort/sorting.py
@@ -2,19 +2,47 @@
 import sys
 import random
 
-def partition3(a, l, r):
-    #write your code here
-    pass
 
-def partition2(a, l, r):
-    x = a[l]
-    j = l
-    for i in range(l + 1, r + 1):
-        if a[i] <= x:
-            j += 1
-            a[i], a[j] = a[j], a[i]
-    a[l], a[j] = a[j], a[l]
+def swap_left(arr, start, high):
+    i = start
+    j = high
+    while i < j:
+        if arr[i] == arr[j]:
+            arr[i], arr[j - 1] = arr[j - 1], arr[i]
+            j -= 1
+        i += 1
     return j
+
+
+def swap_right(arr, start, high):
+    i = start
+    j = high
+    while i < j:
+        if arr[i] == arr[j]:
+            arr[i + 1], arr[j] = arr[j], arr[i + 1]
+            i += 1
+        j -= 1
+    return i
+
+
+def partition3(array, start, end):
+    pivot = array[start]
+    # low tracks elements greater than pivot
+    low = start + 1
+    # high tracks elements less than pivot
+    high = end
+    while True:
+        while low <= high and array[high] >= pivot:
+            high = high - 1
+        while low <= high and array[low] <= pivot:
+            low = low + 1
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+    array[start], array[high] = array[high], array[start]
+    m1, m2 = swap_left(array, start, high), swap_right(array, high, end)
+    return m1, m2
 
 
 def randomized_quick_sort(a, l, r):
@@ -22,15 +50,15 @@ def randomized_quick_sort(a, l, r):
         return
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
-    #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
+    # use partition3
+    m1, m2 = partition3(a, l, r)
+    randomized_quick_sort(a, l, m1)
+    randomized_quick_sort(a, m2+1, r)
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    randomized_quick_sort(a, 0, n - 1)
-    for x in a:
+    n = int(input())
+    lst = list(map(int, input().split()))
+    randomized_quick_sort(lst, 0, n - 1)
+    for x in lst:
         print(x, end=' ')
